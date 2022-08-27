6dd72499f49a5bbe42b3f663639cd8c9981cf76e
Jacques <20562845+frederickjjoubert@users.noreply.github.com>
2022-08-27 06:47:44 -0700

    combining items system uses combine button to initiate combine and MouseInteractive

--- src/game/combining_system.rs
+++ src/game/combining_system.rs
@@ -5,6 +5,7 @@ use crate::config::data_recipes::RecipesData;
 use crate::game::items::Item;
 use crate::game::recipes::Recipe;
 use crate::game::{find_free_space, SpawnItemEvent};
+use crate::mouse::MouseInteractive;
 use crate::positioning::{Coords, GridData};
 
 use super::items::CraftItem;
@@ -14,6 +15,7 @@ pub struct CombineButton {
     pub coords: Coords,
 }
 
+
 // use events here so this doesn't run once a frame?
 pub fn combine_items_system(
     mut commands: Commands,
@@ -21,38 +23,43 @@ pub fn combine_items_system(
     recipes_data: Res<RecipesData>,
     items_data: Res<ItemsData>,
     grid: Res<GridData>,
+    combine_button_query: Query<&MouseInteractive, With<CombineButton>>,
     crafting_items_query: Query<(Entity, &Item), With<CraftItem>>,
     items_query: Query<&Coords, With<Item>>,
 ) {
-    let number_of_crafting_items = crafting_items_query.iter().count();
-    if number_of_crafting_items <= 1 {
-        return;
-    }
+    if let Ok(combine_button) = combine_button_query.get_single() {
+        if combine_button.clicked {
+            let number_of_crafting_items = crafting_items_query.iter().count();
+            if number_of_crafting_items <= 1 {
+                return;
+            }
 
-    let mut items = Vec::new();
-    for (_, item) in crafting_items_query.iter() {
-        items.push(item.clone());
-    }
+            let mut items = Vec::new();
+            for (_, item) in crafting_items_query.iter() {
+                items.push(item.clone());
+            }
 
-    let possible_recipe = try_get_recipe(&recipes_data, &items[0], &items[1]);
-    trace!("found possible recipe: {:?}", possible_recipe);
+            let possible_recipe = try_get_recipe(&recipes_data, &items[0], &items[1]);
+            trace!("found possible recipe: {:?}", possible_recipe);
 
-    if let Some(recipe) = possible_recipe {
-        // debug!("found recipe: {:?}", recipe);
-        if let Some((dimens, item)) = items_data.try_get_item(recipe.result) {
-            // debug!("got random item: {:?}", item);
+            if let Some(recipe) = possible_recipe {
+                // debug!("found recipe: {:?}", recipe);
+                if let Some((dimens, item)) = items_data.try_get_item(recipe.result) {
+                    // debug!("got random item: {:?}", item);
 
-            if let Some(free_coords) = find_free_space(&grid, dimens, &items_query) {
-                // ^ this is failing
-                debug!("found free space to place the item");
-                // Spawn the result of the recipe
-                spawn_event_writer.send(SpawnItemEvent::new(item, free_coords));
-                // Delete the craft items entities
-                for (entity, _) in crafting_items_query.iter() {
-                    commands.entity(entity).despawn_recursive();
+                    if let Some(free_coords) = find_free_space(&grid, dimens, &items_query) {
+                        // ^ this is failing
+                        debug!("found free space to place the item");
+                        // Spawn the result of the recipe
+                        spawn_event_writer.send(SpawnItemEvent::new(item, free_coords));
+                        // Delete the craft items entities
+                        for (entity, _) in crafting_items_query.iter() {
+                            commands.entity(entity).despawn_recursive();
+                        }
+                    } else {
+                        warn!("Tried to find free space but failed.");
+                    }
                 }
-            } else {
-                warn!("Tried to find free space but failed.");
             }
         }
     }
